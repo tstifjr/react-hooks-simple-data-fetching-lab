abfbeb5acb84b7172646293c2546dcbc97077eb5
TJ Stifter <tjstifter@gmail.com>
2023-06-16 12:57:03 -0400

    completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [image, setImage] = useState('')
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r => r.json())
+            .then(data => setImage(data.message))
+    }, [])
+    const renderDisplay = (image === "") ? <p>Loading...</p> : <img src={image} alt="A Random Dog" />
+    console.log(image);
+    return (
+        <div>
+            {renderDisplay}
+        </div>
+    )
+}
+
+export default App;
